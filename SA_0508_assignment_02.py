022732dfcc350877f9e8d44b010b85ad1371e033
BeautyJingMac <kidmac5321@gmail.com>
2023-05-08 17:56:26 +0800

    ##chatGPT教說要怎麼計算程式距離

---
+++ SA_0508_assignment_02.py
@@ -0,0 +1,31 @@
+# chatGPT 告訴的計算程式距離方法
+
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+cities = ['A', 'B', 'C', 'D', 'E']
+#
+DMatrix = np.array([[0, 1, 2, 3, 4, 5],
+                    [1, 0, 5, 8, 10, 12],
+                    [2, 5, 0, 20, 10, 5],
+                    [3, 8, 20, 0, 15, 2],
+                    [4, 10, 10, 15, 0, 3],
+                    [5, 12, 5, 2, 3, 0]])
+
+# 計算 a-b-c-d-e-a 的總距離
+path = ['A', 'B', 'C', 'D', 'E', 'A']
+
+
+total_distance = 0
+for i in range(len(path) - 1):
+    this_city = path[i]
+    next_city = path[i + 1]
+    index_this = cities.index(this_city)
+    index_next = cities.index(next_city)
+    distance = DMatrix[index_this][index_next]
+    total_distance += distance
+
+# 顯示結果
+print(f"a-b-c-d-e-a 的總距離為 {total_distance}")
